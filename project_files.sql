a03df2d037aa1105469f86a8bfc2b44e6a23765a
dani-yalwaseem <dani.dani6970@gmail.com>
2025-05-13 23:24:14 +0500

    updated

--- project_files.sql
+++ project_files.sql
@@ -144,4 +144,80 @@ FROM
 WHERE rank <= 10
 
 
+-- in my SQL Server Following will be the queries 
+
+-- Q.1 List all distinct users and their stats (return user_name, total_submissions, points earned)
+
+SELECT 
+	username,
+	COUNT(id) AS total_submissions,
+	SUM(points) AS points_earned
+FROM user_submissions
+GROUP BY username
+ORDER BY total_submissions DESC;
+
+-- Q.2 Calculate the daily average points for each user.
+
+SELECT 
+	DATE_FORMAT(submitted_at, '%d-%m') AS day,
+	username,
+	AVG(points) AS daily_avg_points
+FROM user_submissions
+GROUP BY day, username
+ORDER BY username;
+
+-- Q.3 Find the top 3 users with the most correct submissions for each day.
+
+WITH daily_submissions AS (
+	SELECT 
+		DATE(submitted_at) AS daily,
+		username,
+		SUM(CASE WHEN points > 0 THEN 1 ELSE 0 END) AS correct_submissions
+	FROM user_submissions
+	GROUP BY daily, username
+),
+ranked_users AS (
+	SELECT 
+		daily,
+		username,
+		correct_submissions,
+		DENSE_RANK() OVER (PARTITION BY daily ORDER BY correct_submissions DESC) AS rank
+	FROM daily_submissions
+)
+SELECT 
+	daily,
+	username,
+	correct_submissions
+FROM ranked_users
+WHERE rank <= 3;
+
+
+-- Q.4 Find the top 5 users with the highest number of incorrect submissions.
+
+SELECT 
+	username,
+	SUM(CASE WHEN points < 0 THEN 1 ELSE 0 END) AS incorrect_submissions,
+	SUM(CASE WHEN points > 0 THEN 1 ELSE 0 END) AS correct_submissions,
+	SUM(CASE WHEN points < 0 THEN points ELSE 0 END) AS incorrect_submissions_points,
+	SUM(CASE WHEN points > 0 THEN points ELSE 0 END) AS correct_submissions_points_earned,
+	SUM(points) AS points_earned
+FROM user_submissions
+GROUP BY username
+ORDER BY incorrect_submissions DESC
+LIMIT 5;
+
+-- Q.5 Find the top 10 performers for each week.
+
+WITH weekly_scores AS (
+	SELECT 
+		WEEK(submitted_at) AS week_no,
+		username,
+		SUM(points) AS total_points_earned,
+		DENSE_RANK() OVER (PARTITION BY WEEK(submitted_at) ORDER BY SUM(points) DESC) AS rank
+	FROM user_submissions
+	GROUP BY week_no, username
+)
+SELECT * FROM weekly_scores
+WHERE rank <= 10;
+
 
